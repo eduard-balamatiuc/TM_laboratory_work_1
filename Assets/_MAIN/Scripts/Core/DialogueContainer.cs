5b85680c3f566eb3e810ba92646b3c2224643ddc
pyramixofficial <balamatiuc2@gmail.com>
2024-02-15 22:21:03 +0200

    new stuff

---
+++ Assets/_MAIN/Scripts/Core/DialogueContainer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class DialogueContainer : MonoBehaviour
+{
+    public GameObject root;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+}
---
+++ Assets/_TESTING/Scripts/Testing_Architect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TESTING
+{
+    public class Testing_Architect : MonoBehaviour
+    {
+        DialogueSystem ds;
+        TextArchitect architect;
+        // Start is called before the first frame update
+        void Start()
+        {
+            ds = DialogueSystem.instance;
+            architect = new TextArchitect(ds.dialogueContainer.dialogueText);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
